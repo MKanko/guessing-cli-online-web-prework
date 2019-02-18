776f1c84927d31c11263992cdcd53f966b241a57
Mark Kanko <mkanko66@gmail.com>
2019-02-18 01:16:11 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,6 +1,5 @@
-require 'pry'
-
 def run_guessing_game
+   
   puts "Guess a number between 1 and 6."  
   user_input = gets.chomp
   correct_number = rand(1..6) 
@@ -38,6 +37,7 @@ end
 #     end    
 #   end 
 # end 
+
 
 
 
